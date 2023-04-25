99b09c85697584d1f5441bd3768d9fe4f94f9f83
bielgennaro <zirondigabriel@gmail.com>
2023-04-25 03:31:03 -0300

    correção no método POST para cadastro de vendedor

--- src/main/java/com/xbrain/Controllers/VendedorController.java
+++ src/main/java/com/xbrain/Controllers/VendedorController.java
@@ -3,25 +3,13 @@ package com.xbrain.Controllers;
 import com.xbrain.Service.VendedorService;
 import com.xbrain.Vendedor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.List;
-
 @RestController
-@RequestMapping(value = "/vendedor", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/vendedor", method = RequestMethod.POST)
 public class VendedorController {
     @Autowired
     private VendedorService vendedorService;
-
-    @GetMapping("/total-media-vendas")
-    public List<Object[]> findVendedoresComTotalEMediaDeVendas(
-            @RequestParam("data-inicial") @DateTimeFormat(pattern = "dd-MM-yyyy") Date dataInicial,
-            @RequestParam("data-final") @DateTimeFormat(pattern = "dd-MM-yyyy") Date dataFinal) {
-        return vendedorService.findVendedoresComTotalEMediaDeVendas(dataInicial, dataFinal);
-    }
 
     @PostMapping("/cadastrar")
     public void cadastrarVendedor(@RequestBody Vendedor vendedor) {
