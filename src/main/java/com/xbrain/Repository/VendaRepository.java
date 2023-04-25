b96d9abb283397b9f14733ae4a4da80d8f3d5e7c
bielgennaro <zirondigabriel@gmail.com>
2023-04-25 03:32:05 -0300

    Modificações no método List

--- src/main/java/com/xbrain/Repository/VendaRepository.java
+++ src/main/java/com/xbrain/Repository/VendaRepository.java
@@ -4,11 +4,10 @@ import com.xbrain.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface VendaRepository extends JpaRepository<Venda, Integer> {
-    List<Venda> findByVendedorIdAndDataBetween(int vendedor_id, LocalDateTime data, LocalDateTime data2);
+    List<Venda> findByVendedorIdAndDataBetween(Integer vendedor_id, LocalDateTime data, LocalDateTime data2);
 }
