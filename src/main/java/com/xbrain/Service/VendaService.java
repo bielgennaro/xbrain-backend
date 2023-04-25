04fcfd1645c2b2246df1b20e1004646128aed65d
bielgennaro <zirondigabriel@gmail.com>
2023-04-25 03:33:08 -0300

    Correção de identação

--- src/main/java/com/xbrain/Service/VendaService.java
+++ src/main/java/com/xbrain/Service/VendaService.java
@@ -6,18 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 
 @Service
 public class VendaService {
 
+
     @Autowired
     private VendaRepository vendaRepository;
 
-    public List<Venda> findByVendedorIdAndDataBetween(int vendedor_id, LocalDateTime data, LocalDateTime data2){
-        return vendaRepository.findByVendedorIdAndDataBetween(vendedor_id, data, data2);
-    }
 
     public void criarVenda(Venda venda) {
         venda.setData(LocalDateTime.now());
